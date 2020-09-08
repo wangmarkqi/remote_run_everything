fd87788e72432db26562dbd0e90d4e9d05461d3a
wangmarkqi <wangalexqi@126.com>
2020-09-08 14:10:16 +0800

    show print

--- remote_run_everything/local.py
+++ remote_run_everything/local.py
@@ -1,12 +1,10 @@
 import os
-from remote_run_everything.conf import Conf
 from remote_run_everything.remote import Remote
 
 
 class Local:
     def __init__(self, conf):
         self.c = conf
-        self.r = Remote(conf)
     
     def get_local(self,dir):
         res = []
@@ -16,28 +14,32 @@ class Local:
                 res.append(e.replace("\\", "/"))
         return res
     
-    
-    
     def upload(self,dir):
+        r = Remote(self.c)
         files = self.get_local(dir)
         local_len = len(self.c.local_root)
+        data=[]
         for lf in files:
             relativ_loc = lf[local_len:]
             if not relativ_loc.startswith('/'):
                 relativ_loc = "/" + relativ_loc
             rf = self.c.remote_root + relativ_loc
-            self.r.upload(lf, rf)
+            data.append((lf,rf))
+        r.upload(data)
         return "success"
     
     def download(self,dir):
-        files = self.r.get_remote(dir)
+        r = Remote(self.c)
+        files = r.get_remote(dir)
         remote_len = len(self.c.remote_root)
+        data=[]
         for rf in files:
             relative_loc = rf[remote_len:]
             if not relative_loc.startswith("/"):
                 relative_loc= "/" + relative_loc
             lf = self.c.local_root + relative_loc
-            self.r.download(lf,rf)
+            data.append((lf,rf))
+        r.download(data)
         return "success"
 
 
--- remote_run_everything/remote.py
+++ remote_run_everything/remote.py
@@ -1,44 +1,47 @@
 import pysftp, os
-from remote_run_everything.conf import Conf
 
 class Remote:
     def __init__(self,conf):
         self.c=conf
-        self.shell = pysftp.Connection(self.c.host, username=self.c.user, password=self.c.pwd)
-    def __del__(self):
-        if self.shell!=None:
-            self.shell.close()
 
-    def upload(self, local_f, remote_f):
-        remote_dir = os.path.dirname(remote_f)
-        self.shell.makedirs(remote_dir, mode=777)
-        self.shell.put(local_f, remote_f)
+    def upload(self, data):
+        with pysftp.Connection(self.c.host, username=self.c.user, password=self.c.pwd) as shell:
+            for i in data:
+                remote_dir = os.path.dirname(i[1])
+                shell.makedirs(remote_dir, mode=777)
+                print(i[0], "------------>", i[1])
+                shell.put(i[0], i[1])
     
-    def download(self, local_f, remote_f):
-        local_dir = os.path.dirname(local_f)
-        os.makedirs(local_dir, exist_ok=True, mode=777)
-        self.shell.get(remote_f, local_f)
+    def download(self, data):
+        with pysftp.Connection(self.c.host, username=self.c.user, password=self.c.pwd) as shell:
+            for i in data:
+                local_dir = os.path.dirname(i[0])
+                os.makedirs(local_dir, exist_ok=True, mode=777)
+                print(i[0], "<------------", i[1])
+                shell.get(i[1], i[0])
     
     def get_remote(self,dir):
         files=self._get_remote(dir,[])
         return files
     
     def _get_remote(self,root,res):
-        data=self.shell.listdir(root)
-        for d in data:
-            new=os.path.join(root,d).replace("\\","/")
-            if self.shell.isfile(new):
-                res.append(new)
-            else:
-                self._get_remote(new,res)
+        with pysftp.Connection(self.c.host, username=self.c.user, password=self.c.pwd) as shell:
+            data=shell.listdir(root)
+            for d in data:
+                new=os.path.join(root,d).replace("\\","/")
+                if shell.isfile(new):
+                    res.append(new)
+                else:
+                    self._get_remote(new,res)
         return res
     def cmd(self,cmds):
-        common_cmds = ["/usr/bin/bash -c", f"cd {self.c.remote_root}"]
-        all=common_cmds+cmds
-        cmd=";".join(all)
-        res = self.shell.execute(cmd)
-        print("***********************")
-        for i in res:
-            print(i.decode("utf-8"))
+        with pysftp.Connection(self.c.host, username=self.c.user, password=self.c.pwd) as shell:
+            common_cmds = ["/usr/bin/bash -c", f"cd {self.c.remote_root}"]
+            all=common_cmds+cmds
+            cmd=";".join(all)
+            res = shell.execute(cmd)
+            print("***********************")
+            for i in res:
+                print(i.decode("utf-8"))
 
 
